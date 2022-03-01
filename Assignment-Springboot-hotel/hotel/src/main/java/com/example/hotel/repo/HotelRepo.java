8459b115bf05c656cd154a15b8e67368463723dc
Jongwon Shinn <90646772+A00407059@users.noreply.github.com>
2022-03-01 14:15:42 -0400

    Delete Assignment-Springboot-hotel/hotel directory

--- Assignment-Springboot-hotel/hotel/src/main/java/com/example/hotel/repo/HotelRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.hotel.repo;
-
-import com.example.hotel.models.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepo extends JpaRepository<Hotel, Long> {
-}
