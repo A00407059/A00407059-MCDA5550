3bf31293d47eed2587da532d2952800c460a02b6
Jongwon Shinn <90646772+A00407059@users.noreply.github.com>
2022-03-09 15:40:51 -0400

    Add files via upload

---
+++ Assignment-Django-hotel/hotelapp/urls.py
@@ -0,0 +1,11 @@
+"""Create every time you creat app"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path("home/", views.home, name="home"),
+    path("hotel_list/", views.Hotels_list, name="hotelList"),
+    path("hotel_list/<str:pk>", views.Hotels_list, name="hotelList"),
+]
